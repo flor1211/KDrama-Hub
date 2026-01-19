35be11b2f77b2dac4d3dee10f656707f70fbf4b8
flor <florenciomendozarosalesjr@gmail.com>
2026-01-19 09:21:23 +0800

    Enable Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/react"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
         <App />
+        <Analytics />
     </BrowserRouter>
   </StrictMode>,
 )
